5bb245e19372f20cd08c18ca6e62e0a27f64b111
Sun4n <kazisunan68@gmail.com>
2026-07-29 20:18:15 +0600

    build delete modal section

--- src/component/shared/DeleteIdea.jsx
+++ src/component/shared/DeleteIdea.jsx
@@ -1,16 +1,53 @@
+'use client'
 import { AlertDialog, Button } from '@heroui/react';
+import { redirect } from 'next/navigation';
 import React from 'react';
+import toast from 'react-hot-toast';
 import { MdDelete } from 'react-icons/md';
 
-const DeleteIdea = () => {
+const DeleteIdea = ({data}) => {
     const {_id}=data
     const handleDelete = async()=>{
         const res = await fetch(`http://localhost:5000/idea/${_id}`,{
             method:'DELETE',
         })
+        const data = await res.json()
+        console.log(data);
+        if (data.deletedCount>0) {
+            toast.error('Idea Delete')
+            redirect('/my-idea')
+        }
     }
     return (
-        
+        <AlertDialog>
+            <Button variant="danger"><MdDelete /></Button>
+            <AlertDialog.Backdrop>
+                <AlertDialog.Container>
+                    <AlertDialog.Dialog className="sm:max-w-[400px]">
+                        <AlertDialog.CloseTrigger />
+                        <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>Delete project permanently?</AlertDialog.Heading>
+                        </AlertDialog.Header>
+                        <AlertDialog.Body>
+                            <p>
+                                This will permanently delete <strong>My Awesome Project</strong> and all of its
+                                data. This action cannot be undone.
+                            </p>
+                        </AlertDialog.Body>
+                        <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary">
+                                Cancel
+                            </Button>
+                            <Button onClick={handleDelete} slot="close" variant="danger">
+                                Delete Project
+                            </Button>
+                        </AlertDialog.Footer>
+                    </AlertDialog.Dialog>
+                </AlertDialog.Container>
+            </AlertDialog.Backdrop>
+        </AlertDialog>
+
     );
 };
 
